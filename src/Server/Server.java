90e67333ae51ab649803ec6a20019cc03a7253d4
Aanchal Mahajan <aanchal.mahajan93@gmail.com>
2018-02-21 12:33:48 -0700

    Chanel and frequency values read

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -9,7 +9,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
 
-public class Server {
+public class Server  {
 
     static Server ServerInstance = new Server();
     private boolean ServerStatus ;
@@ -17,6 +17,10 @@ public class Server {
     PrintStream outToClient=null;
     BufferedReader buffer=null;
     ServerSocket listener=null;
+    int highest_value = 1024;
+    int lowest_value = 0;
+    int no_of_channels = 2;
+    int frequency =2;
     public static Server getServerInstance()
     {
         return ServerInstance ;
@@ -47,11 +51,9 @@ public class Server {
 
     public void StartServer () {
 
-            int highest_value = 1024;
-            int lowest_value = 0;
+            
             int stream[] = new int[100];
-            int no_of_channels = 2;
-            int frequency = 2;
+            
             Random randomNumber = new Random();
             try{
                 listener = new ServerSocket(1516);
@@ -74,40 +76,41 @@ public class Server {
                         buffer = new BufferedReader(new InputStreamReader(System.in));
 
 
-                        while(true){
+                      /*  while(true){
                             Integer num = lowest_value + randomNumber.nextInt(highest_value);
                             outToClient.println(num);
 
-                        }
-//                        Thread thread = new Thread(new Runnable() {
-//                            public void run() {
-//                                while (true) {
+                        }*/
+                        
+                        
+                        frequency= buffer.read(); 
+                        no_of_channels= buffer.read();
+                        
+                        Thread thread = new Thread(new Runnable() {
+                            public void run() {
+                                while (true) {
 
-//                                    for (int j = 1; j <= frequency; j++) {
-//                                        for (int i = 1; i <= no_of_channels; i++) {
-//                                            stream[i] = lowest_value + randomNumber.nextInt(highest_value);
-//                                        }
-//                                        for (int i = 1; i <= no_of_channels; i++) {
-//                                            try {
-//                                                System.out.print(stream[i]);
-//                                                outToClient.writeInt(stream[i]);
-//                                            } catch (IOException e) {
-//                                                e.printStackTrace();
-//                                            }
-//                                        }
-//                                    }
+                                    for (int j = 1; j <= frequency; j++) {
+                                        for (int i = 1; i <= no_of_channels; i++) {
+                                            stream[i] = lowest_value + randomNumber.nextInt(highest_value);
+                                        }
+                                        for (int i = 1; i <= no_of_channels; i++) {
+                                            System.out.print(stream[i]);
+											outToClient.write(stream[i]);
+                                        }
+                                    }
 
 
-//                                    try {
-//                                        Thread.sleep(1000);
-//                                    } catch (InterruptedException e) {
-//                                        // TODO Auto-generated catch block
-//                                        e.printStackTrace();
-//                                    }
-//                                }
-//                            }
-//                        });
-//                        thread.start();
+                                    try {
+                                        Thread.sleep(1000);
+                                    } catch (InterruptedException e) {
+                                        // TODO Auto-generated catch block
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        });
+                        thread.start();
 
 
 
@@ -125,4 +128,7 @@ public class Server {
                 }
             }
     }
+
+
+
 }
