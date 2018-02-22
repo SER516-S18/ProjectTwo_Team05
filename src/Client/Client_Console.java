f0c2dd406221a625aa82e9a376e43eb7694f840a
unknown <abhis.kumar99@gmail.com>
2018-02-22 14:31:11 -0700

    Console client constructor

---
+++ src/Client/Client_Console.java
@@ -0,0 +1,21 @@
+package Client;
+
+
+import javax.swing.JTextPane;
+
+
+
+public class Client_Console {
+
+
+
+	JTextPane consoleDisplay;
+
+	private static Console console = null;
+
+
+	public Client_Console() {
+
+//add contsructor
+
+	}
