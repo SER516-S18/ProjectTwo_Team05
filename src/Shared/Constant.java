399031b76437f0663fa2af0a28bb1aa1052550fd
mlaad <mlaad@asu.edu>
2018-02-22 00:49:53 -0700

    added standard constants

--- src/Shared/Constant.java
+++ src/Shared/Constant.java
@@ -1,7 +1,12 @@
 package Shared;
 
+import java.awt.Color;
+
 public class Constant {
 
 	public static final int PORT_NUMBER = 1516;
+	public static final String HOST_NAME = "localhost";
+	public static final Color LIGHTBLUE = new Color(173,216,230);
+	public static final Color LIGHTPINK = new Color(255,182,193);
 	
 }
