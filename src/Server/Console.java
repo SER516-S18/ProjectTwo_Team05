ac8f09add3dadb28c42b6bc40c6790692eae168c
unknown <abhis.kumar99@gmail.com>
2018-02-22 09:34:32 -0700

    Console Server constructor added

---
+++ src/Server/Console.java
@@ -0,0 +1,23 @@
+package Server;
+
+
+import javax.swing.JTextPane;
+
+
+
+public class Console {
+
+
+
+	JTextPane consoleDisplay;
+
+	private static Console console = null;
+
+
+	public Console() {
+
+//add contructor
+
+	}
+	
+}
