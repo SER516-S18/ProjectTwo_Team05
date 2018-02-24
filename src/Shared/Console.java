657d8fceea38dec2cd4e2124acd259a3b251184d
Abhishek Kumar <abhis.kumar99@gmail.com>
2018-02-23 20:13:59 -0700

    moving console to shared

---
+++ src/Shared/Console.java
@@ -0,0 +1,22 @@
+package Shared;
+
+import javax.swing.JTextPane;
+
+public class Console {
+    
+	protected JTextPane consolePanel;
+
+	/**
+	 * @param consolePanel
+	 */
+	public void setConsolePanel(JTextPane consolePanel) {
+        this.consolePanel = consolePanel;
+    }
+	
+	 /**
+	 * @param inputMsg
+	 */
+	public void display(String inputMsg) {
+	        this.consolePanel.setText("Console:" + inputMsg);
+	    }
+}
