8e14c9dd282c0ba91f59daf7feaa7f4ea9c1c9f2
Mitali Kulkarni <kulkarnimitali20@gmail.com>
2018-02-23 20:51:49 -0700

    Merge code: Format code

--- src/Shared/Console.java
+++ src/Shared/Console.java
@@ -3,20 +3,20 @@ package Shared;
 import javax.swing.JTextPane;
 
 public class Console {
-    
+
 	protected JTextPane consolePanel;
 
 	/**
 	 * @param consolePanel
 	 */
 	public void setConsolePanel(JTextPane consolePanel) {
-        this.consolePanel = consolePanel;
-    }
-	
-	 /**
+		this.consolePanel = consolePanel;
+	}
+
+	/**
 	 * @param inputMsg
 	 */
 	public void display(String inputMsg) {
-	    this.consolePanel.setText("Console:" + inputMsg);
+		this.consolePanel.setText("Console:" + inputMsg);
 	}
 }
