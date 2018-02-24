a695f6d6899a903fa47dd6e860aa09c27af1773e
Abhishek Kumar <abhis.kumar99@gmail.com>
2018-02-23 20:28:48 -0700

    Changing ClientUI

--- src/Server/ServerConsole.java
+++ src/Server/ServerConsole.java
@@ -1,43 +1,27 @@
-/**
- * @SER516 Project2_Team05
- */
-
 package Server;
 
-import javax.swing.JLabel;
 
-/**
- * ServerConsole class is used to display the messages on the console
- **/
-public class ServerConsole {
+import java.awt.*;
+import javax.swing.*;
 
-	private static ServerConsole console = null;
-	JLabel consoleDisplay;
+import Shared.Console;
 
-	/**
-	 * print method is printing the value to the console 
-	 * @param text to be print
-	 **/
-	public void print(String text) {
-		this.consoleDisplay.setText(text);
-	}
 
-	/**
-	 * ServerConsole method is creating a new instance of the class if we have not created
-	 * @return console
-	 **/
-	public static ServerConsole getConsole() {
-		if (null == console) {
-			console = new ServerConsole();
-		}
-		return console;
-	}
+public class ServerConsole extends Console{
 
-	/**
-	 * setConsole method sets the newly created console to the this.consoleDisplay
-	 * @param console
-	 **/
-	public void setConsole(JLabel console) {
-		this.consoleDisplay = console;
-	}
+	
+	   private static ServerConsole serverConsole = null;
+
+	   
+	    public static ServerConsole getServerConsole() {
+	       
+	    	if (serverConsole == null)
+	    	{
+	            serverConsole = new ServerConsole();
+	        }
+	        return serverConsole;
+	    }
+	    
+
+
 }
